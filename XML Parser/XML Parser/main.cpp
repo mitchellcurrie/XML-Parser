1cf6f1bc1314d7724de48b1a11f1f28563d6b688
MDS\mitchell.cur6577 <mitchell.currie@hotmail.com>
2017-05-29 11:25:46 +1200

    Convert from CSV to XML

--- XML Parser/XML Parser/main.cpp
+++ XML Parser/XML Parser/main.cpp
@@ -8,35 +8,104 @@ using namespace std;
 void CreateXMLFile();
 void CreateCSVFile();
 void ConvertFromXMLtoCSV();
+void ConvertFromCSVtoXML();
 bool IsTitleAdded(vector<string> _vector, string _strToCheck);
 void AddCSVRow(ofstream& _file, vector<string> _vector);
-
+void AddXMLSection(ofstream& _file, vector<string> _titleVector, vector<string> _gameInfoVector, string _child);
 
 
 int main()
 {
 	cout << "XML Parser" << endl;
 
-	CreateXMLFile();
+//	CreateXMLFile();
 //	CreateCSVFile();
 
-	ConvertFromXMLtoCSV();
+//	ConvertFromXMLtoCSV();
+	ConvertFromCSVtoXML();
 
 	int i = 0;
 	cin >> i;
 }
 
-/*
+void ConvertFromCSVtoXML()
+{
+	string filename;
+	cout << "Enter the filename of the CSV file to convert to XML: ";
+	getline(cin, filename);
 
-	Title					Year		Genre					Platform		Developer
-	Uncharted 2				2009		Action-Adventure		PS3				Naughty Dog
-	Red Dead Redemption		2010		Action-Adventure		PS3				Rockstar San Diego
-	Dirt 3					2011		Racing					PS3				Codemasters
-	GTA V					2013		Action-Adventure		PS3				Rockstar North
-	COD Advanced Warfare	2014		FPS						XBOX One		Sledgehammer Games
+	string xmlFilename = filename.substr(0, filename.find_first_of('.')) + ".xml";
+	ofstream xmlFile;
+	xmlFile.open(xmlFilename);
 
-*/
+	ifstream csvFile(filename);
+	string line;
+	string data;
+	string root = "rootNode";
+	string child = "child";
+	vector <string> titles;
+	vector <string> gameInfo;
+	bool bTitlesSet = false;
+	bool bRowSet = false;
 
+	xmlFile << "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n\n";
+	xmlFile << "<" << root << ">\n\n";
+
+	if (csvFile.is_open())
+	{			
+		(getline(csvFile, line));
+
+		while (!bTitlesSet)
+		{
+			if (line.find(',') != string::npos)
+			{
+				data = line.substr(0, line.find_first_of(","));
+				line.erase(0, line.find_first_of(",") + 1);
+				if (line[0] == ' ')
+					line.erase(0, 1);
+			}	
+			else
+			{ 
+				data = line.substr(0, line.length());
+				bTitlesSet = true;
+			}
+
+			titles.push_back(data);
+		}
+
+		while (getline(csvFile, line))
+		{
+			while (!bRowSet)
+			{
+				if (line.find(',') != string::npos)
+				{
+					data = line.substr(0, line.find_first_of(","));
+					line.erase(0, line.find_first_of(",") + 1);
+					if (line[0] == ' ')
+						line.erase(0, 1);
+				}
+				else
+				{
+					data = line.substr(0, line.length());
+					bRowSet = true;
+				}
+
+				gameInfo.push_back(data);
+			}
+
+			AddXMLSection(xmlFile, titles, gameInfo, child);
+			gameInfo.clear();
+			bRowSet = false;		
+		}
+
+		xmlFile << "</" << root << ">\n\n";
+	}
+	else
+	{
+		cout << "Could not open csv file";
+	}
+
+}
 void ConvertFromXMLtoCSV()
 {
 	string filename;
@@ -49,9 +118,11 @@ void ConvertFromXMLtoCSV()
 
 	ifstream xmlFile(filename);
 	string line;
+	string firstTitle;
 	string groupTitle;
 	vector <string> rowText;
-	
+
+	bool bPassedFirstTitle = false;
 	bool bGroupTitleSet = false;
 
 	if (xmlFile.is_open())
@@ -62,13 +133,22 @@ void ConvertFromXMLtoCSV()
 			// Set the group title - ignores blank lines and set up lines with "?" at the second to last position
 			if ((line != "") && !bGroupTitleSet && (line.at(line.length()-2) != '?'))
 			{
-				groupTitle = line.substr(1, line.length() -2);
-				bGroupTitleSet = true;
+				if (bPassedFirstTitle)
+				{
+					groupTitle = line.substr(1, line.length() - 2);
+					bGroupTitleSet = true;
+				}
+				else
+				{
+					firstTitle = line.substr(1, line.length() - 2);
+					bPassedFirstTitle = true;
+				}			
 			}		
 			else if (bGroupTitleSet)
 			{
-				// Ignore blank lines, set up lines (as above), and lines with the group title found above
-				if ((line != "") && (line.at(line.length() - 2) != '?') && (line.find(groupTitle) == string::npos))
+				// Ignore blank lines, set up lines (as above), and lines with the group title and first title found above
+				if ((line != "") && (line.at(line.length() - 2) != '?') && (line.find(groupTitle) == string::npos) &&
+					(line.find(firstTitle) == string::npos))
 				{
 					//string title = line.substr(1, line.find_first_of('>') - 1);
 
@@ -104,7 +184,8 @@ void ConvertFromXMLtoCSV()
 				iTitleCounter = 0;
 			}
 			
-			if ((line != "") && (line.at(line.length() - 2) != '?') && (line.find(groupTitle) == string::npos))
+			if ((line != "") && (line.at(line.length() - 2) != '?') && (line.find(groupTitle) == string::npos) &&
+				(line.find(firstTitle) == string::npos))
 			{
 				string data = line.substr((line.find_first_of('>') + 2), (line.size() - 1));
 				data = data.substr(0, data.find_last_of(' '));
@@ -121,7 +202,6 @@ void ConvertFromXMLtoCSV()
 		cout << "Could not open xml file";
 	}
 }
-	
 void CreateXMLFile()
 {
 	ofstream xmlFile;
@@ -129,45 +209,47 @@ void CreateXMLFile()
 	xmlFile << "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n\n";
 
 	// Input favourite game information
-	xmlFile << "<game>\n";
-		xmlFile << "<title> Uncharted 2 </title>\n";
-		xmlFile << "<year> 2009 </year>\n";
-		xmlFile << "<genre> Action-Adventure </genre>\n";
-		xmlFile << "<platform> PS3 </platform>\n";
-		xmlFile << "<developer> Naughty Dog </developer>\n";
-	xmlFile << "</game>\n\n";
+	xmlFile << "<favouriteGames>\n\n";
+		xmlFile << "<game>\n";
+			xmlFile << "<title> Uncharted 2 </title>\n";
+			xmlFile << "<year> 2009 </year>\n";
+			xmlFile << "<genre> Action-Adventure </genre>\n";
+			xmlFile << "<platform> PS3 </platform>\n";
+			xmlFile << "<developer> Naughty Dog </developer>\n";
+		xmlFile << "</game>\n\n";
 
-	xmlFile << "<game>\n";
-		xmlFile << "<title> Red Dead Redemption </title>\n";
-		xmlFile << "<year> 2010 </year>\n";
-		xmlFile << "<genre> Action-Adventure </genre>\n";
-		xmlFile << "<platform> PS3 </platform>\n";
-		xmlFile << "<developer> Rockstar San Diego </developer>\n";
-	xmlFile << "</game>\n\n";
+		xmlFile << "<game>\n";
+			xmlFile << "<title> Red Dead Redemption </title>\n";
+			xmlFile << "<year> 2010 </year>\n";
+			xmlFile << "<genre> Action-Adventure </genre>\n";
+			xmlFile << "<platform> PS3 </platform>\n";
+			xmlFile << "<developer> Rockstar San Diego </developer>\n";
+		xmlFile << "</game>\n\n";
 
-	xmlFile << "<game>\n";
-		xmlFile << "<title> Dirt 3 </title>\n";
-		xmlFile << "<year> 2011 </year>\n";
-		xmlFile << "<genre> Racing </genre>\n";
-		xmlFile << "<platform> PS3 </platform>\n";
-		xmlFile << "<developer> Codemasters </developer>\n";
-	xmlFile << "</game>\n\n";
+		xmlFile << "<game>\n";
+			xmlFile << "<title> Dirt 3 </title>\n";
+			xmlFile << "<year> 2011 </year>\n";
+			xmlFile << "<genre> Racing </genre>\n";
+			xmlFile << "<platform> PS3 </platform>\n";
+			xmlFile << "<developer> Codemasters </developer>\n";
+		xmlFile << "</game>\n\n";
 
-	xmlFile << "<game>\n";
-		xmlFile << "<title> GTA V </title>\n";
-		xmlFile << "<year> 2013 </year>\n";
-		xmlFile << "<genre> Action-Adventure </genre>\n";
-		xmlFile << "<platform> PS3 </platform>\n";
-		xmlFile << "<developer> Rockstar North </developer>\n";
-	xmlFile << "</game>\n\n";
+		xmlFile << "<game>\n";
+			xmlFile << "<title> GTA V </title>\n";
+			xmlFile << "<year> 2013 </year>\n";
+			xmlFile << "<genre> Action-Adventure </genre>\n";
+			xmlFile << "<platform> PS3 </platform>\n";
+			xmlFile << "<developer> Rockstar North </developer>\n";
+		xmlFile << "</game>\n\n";
 
-	xmlFile << "<game>\n";
-		xmlFile << "<title> COD Advance Warfare </title>\n";
-		xmlFile << "<year> 2014 </year>\n";
-		xmlFile << "<genre> FPS </genre>\n";
-		xmlFile << "<platform> Xbox One </platform>\n";
-		xmlFile << "<developer> Sledgehammer Games </developer>\n";
-	xmlFile << "</game>\n\n";
+		xmlFile << "<game>\n";
+			xmlFile << "<title> COD Advance Warfare </title>\n";
+			xmlFile << "<year> 2014 </year>\n";
+			xmlFile << "<genre> FPS </genre>\n";
+			xmlFile << "<platform> Xbox One </platform>\n";
+			xmlFile << "<developer> Sledgehammer Games </developer>\n";
+		xmlFile << "</game>\n\n";
+	xmlFile << "</favouriteGames>\n";
 
 	xmlFile.close();
 }
@@ -205,3 +287,27 @@ void AddCSVRow(ofstream& _file, vector<string> _vector)
 
 	_file << "\n";
 }
+void AddXMLSection(ofstream& _file, vector<string> _titleVector, vector<string> _gameInfoVector, string _child)
+{
+	size_t NumberOfItems = _titleVector.size();
+	
+	_file << "\t<" << _child << ">\n";
+	
+	for (size_t x = 0; x < NumberOfItems; x++)
+	{
+		_file << "\t\t<" << _titleVector[x] << "> " << _gameInfoVector[x] << " </" << _titleVector[x] << ">\n";
+	}
+
+	_file << "\t</" << _child << ">\n\n";
+}
+
+/*
+
+Title					Year		Genre					Platform		Developer
+Uncharted 2				2009		Action-Adventure		PS3				Naughty Dog
+Red Dead Redemption		2010		Action-Adventure		PS3				Rockstar San Diego
+Dirt 3					2011		Racing					PS3				Codemasters
+GTA V					2013		Action-Adventure		PS3				Rockstar North
+COD Advanced Warfare	2014		FPS						XBOX One		Sledgehammer Games
+
+*/
